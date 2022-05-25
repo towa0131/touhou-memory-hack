ccd59f32813c7dd97abb70b6d007e4c5ebb7cb39
towa0131 <towa.towa0131@gmail.com>
2022-05-25 18:16:35 +0900

    [Change] タブを空白に変換

--- TouhouMemoryHack/memorystream.cpp
+++ TouhouMemoryHack/memorystream.cpp
@@ -10,58 +10,58 @@ MemoryStream::MemoryStream(HANDLE hProcess, int offset) : hProcess(hProcess), of
 }
 
 void MemoryStream::setOffset(int offset) {
-	offset = offset;
+    offset = offset;
 }
 
 int MemoryStream::getOffset() {
-	return offset;
+    return offset;
 }
 
 bool MemoryStream::write(uint8_t byte) {
-	bool ret;
-	uint8_t data[1] = { byte };
-	DWORD oldProtect = changeProtect(sizeof(byte), PAGE_EXECUTE_READWRITE);
-	ret = WriteProcessMemory(hProcess, (LPVOID)offset++, (LPVOID)data, 1, NULL);
-	changeProtect(sizeof(byte), oldProtect);
+    bool ret;
+    uint8_t data[1] = { byte };
+    DWORD oldProtect = changeProtect(sizeof(byte), PAGE_EXECUTE_READWRITE);
+    ret = WriteProcessMemory(hProcess, (LPVOID)offset++, (LPVOID)data, 1, NULL);
+    changeProtect(sizeof(byte), oldProtect);
 
-	return ret;
+    return ret;
 }
 
 bool MemoryStream::write(uint16_t word) {
-	while (word != 0) {
-		uint8_t byte = word % 256;
-		word /= 256;
-		if (!write(byte)) return false;
-	}
-	return true;
+    while (word != 0) {
+        uint8_t byte = word % 256;
+        word /= 256;
+        if (!write(byte)) return false;
+    }
+    return true;
 }
 
 bool MemoryStream::write(uint32_t dword) {
-	while (dword != 0) {
-		uint8_t byte = dword % 0x100;
-		dword /= 0x100;
-		if (!write(byte)) return false;
-	}
-	return true;
+    while (dword != 0) {
+        uint8_t byte = dword % 0x100;
+        dword /= 0x100;
+        if (!write(byte)) return false;
+    }
+    return true;
 }
 
 bool MemoryStream::write(std::vector<uint8_t> bytes) {
-	bool ret;
-	DWORD oldProtect = changeProtect(sizeof(byte), PAGE_EXECUTE_READWRITE);
-	uint8_t* data = bytes.data();
-	ret = WriteProcessMemory(hProcess, (LPVOID)offset, (LPVOID)data, bytes.size(), NULL);
-	changeProtect(sizeof(byte), oldProtect);
-	offset += bytes.size();
+    bool ret;
+    DWORD oldProtect = changeProtect(sizeof(byte), PAGE_EXECUTE_READWRITE);
+    uint8_t* data = bytes.data();
+    ret = WriteProcessMemory(hProcess, (LPVOID)offset, (LPVOID)data, bytes.size(), NULL);
+    changeProtect(sizeof(byte), oldProtect);
+    offset += bytes.size();
 
-	return ret;
+    return ret;
 }
 
 void MemoryStream::close() {
-	CloseHandle(hProcess);
+    CloseHandle(hProcess);
 }
 
 DWORD MemoryStream::changeProtect(int byte, DWORD newProtect) {
-	DWORD oldProtect;
-	VirtualProtectEx(hProcess, (LPVOID)offset, byte, newProtect, &oldProtect);
-	return oldProtect;
+    DWORD oldProtect;
+    VirtualProtectEx(hProcess, (LPVOID)offset, byte, newProtect, &oldProtect);
+    return oldProtect;
 }
--- TouhouMemoryHack/memorystream.h
+++ TouhouMemoryHack/memorystream.h
@@ -1,24 +1,24 @@
 class MemoryStream {
 private:
-	HANDLE hProcess;
-	uint64_t offset;
-	DWORD changeProtect(int byte, DWORD newProtect);
+    HANDLE hProcess;
+    uint64_t offset;
+    DWORD changeProtect(int byte, DWORD newProtect);
 public:
-	MemoryStream(HANDLE hProcess);
+    MemoryStream(HANDLE hProcess);
 
-	MemoryStream(HANDLE hProcess, int offset);
+    MemoryStream(HANDLE hProcess, int offset);
 
-	bool write(uint8_t byte);
+    bool write(uint8_t byte);
 
-	bool write(uint16_t word);
+    bool write(uint16_t word);
 
-	bool write(uint32_t dword);
+    bool write(uint32_t dword);
 
-	bool write(std::vector<uint8_t> bytes);
+    bool write(std::vector<uint8_t> bytes);
 
-	void setOffset(int offset);
+    void setOffset(int offset);
 
-	int getOffset();
+    int getOffset();
 
-	void close();
+    void close();
 };
